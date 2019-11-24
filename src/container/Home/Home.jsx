ae13415a1fa5c30529f9e2a309c6361e125c5ded
Muhammad Fakhran Hadyan <fakhranhadyan@gmail.com>
2019-11-24 20:15:56 +0700

    Hooks

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -7,6 +7,7 @@ import YoutubeCompPage from "../pages/YoutubeCompPage/YoutubeCompPage";
 import DetailPost from "../pages/BlogPost/DetailPost/DetailPost";
 import './Home.css';
 import GlobalProvider from "../../context/context";
+import Hooks from "../pages/Hooks/Hooks";
 
 class Home extends Component{
     render() {
@@ -18,12 +19,14 @@ class Home extends Component{
                         <Link to="/product">Product</Link>
                         <Link to="/youtube-component">YouTube</Link>
                         <Link to="/lifecycle">Life Cycle</Link>
+                        <Link to="/hooks">Hooks</Link>
                     </div>
                     <Route path="/" exact component={BlogPost} />
                     <Route path="/detail-post/:postID" component={DetailPost} />
                     <Route path="/product" component={Product} />
                     <Route path="/lifecycle" component={LifeCycleComp} />
                     <Route path="/youtube-component" component={YoutubeCompPage} />
+                    <Route path="/hooks" component={Hooks} />
                 </Fragment>
             </Router>
         );
